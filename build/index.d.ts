92e28664e7a31187f27790299da09f30f58e2c48
Axel Lavoie <axel.lavoie@gmail.com>
2020-12-28 21:24:17 +0100

    Remove build

--- build/index.d.ts
+++
@@ -1,2 +0,0 @@
-import useScreenSize from './usescreenSize';
-export { useScreenSize };
--- build/usescreenSize.d.ts
+++
@@ -1,7 +0,0 @@
-declare const useScreenSize: () => {
-    innerWidth: number;
-    innerHeight: number;
-    outerWidth: number;
-    outerHeight: number;
-};
-export default useScreenSize;
