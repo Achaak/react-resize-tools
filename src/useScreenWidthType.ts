90cb6ee784d28c9287f8c211322a5dd7e67012fd
Axel Lavoie <axel.lavoie@gmail.com>
2021-12-17 19:32:05 +0100

    update

--- src/useScreenWidthType.ts
+++ src/useScreenWidthType.ts
@@ -1,35 +1,45 @@
 import { useState, useEffect } from "react";
 
 export type UseScreenWidthType = {
-  sm?: number
-  md?: number
-  lg?: number
-  xl?: number
-  xxl?: number
-}
+  sm?: number;
+  md?: number;
+  lg?: number;
+  xl?: number;
+  xxl?: number;
+};
 
-export type WidthType = "xs" | "sm" | "md" | "lg" | "xl" | "xxl" | null
+export type WidthType = "xs" | "sm" | "md" | "lg" | "xl" | "xxl" | null;
 
-const useScreenWidthType = ({ sm=640, md=768, lg=1024, xl=1280, xxl=1400 }: UseScreenWidthType): WidthType => {
-  const [screenWidthType, setScreenWidthType] = useState<WidthType | null>(null);
+const useScreenWidthType = (
+  { sm, md, lg, xl, xxl }: UseScreenWidthType = {
+    sm: 640,
+    md: 768,
+    lg: 1024,
+    xl: 1280,
+    xxl: 1400,
+  }
+): WidthType => {
+  const [screenWidthType, setScreenWidthType] = useState<WidthType | null>(
+    null
+  );
 
   const updateScreenWidth = () => {
-    const width = window.innerWidth
-    let widthType: WidthType = "xs"
+    const width = window.innerWidth;
+    let widthType: WidthType = "xs";
 
-    if(width >= sm)  widthType = "sm"
-    if(width >= md)  widthType = "md"
-    if(width >= lg)  widthType = "lg"
-    if(width >= xl)  widthType = "xl"
-    if(width >= xxl) widthType = "xxl"
+    if (sm && width >= sm) widthType = "sm";
+    if (md && width >= md) widthType = "md";
+    if (lg && width >= lg) widthType = "lg";
+    if (xl && width >= xl) widthType = "xl";
+    if (xxl && width >= xxl) widthType = "xxl";
 
-    if(screenWidthType !== widthType) {
-      setScreenWidthType(widthType)
+    if (screenWidthType !== widthType) {
+      setScreenWidthType(widthType);
     }
   };
 
   useEffect(() => {
-    updateScreenWidth() 
+    updateScreenWidth();
 
     window.addEventListener("resize", updateScreenWidth);
 
